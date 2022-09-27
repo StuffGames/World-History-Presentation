29dac02a9da08760ed2f6792ca17cd2f2863d010
StuffGames <jsanchezroa@umass.edu>
2022-09-27 16:49:38 -0400

    Fifth Commit
    adding skyboxes

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SceneManager : MonoBehaviour
 {
     public EventContentList e;
+    public List<Material> skyboxes;
 
     //If button is selected, turn off until other button selected from the events list
     public void ButtonSelection(GameObject button){
