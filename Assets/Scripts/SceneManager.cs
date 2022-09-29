3b493ff2d9a88f798ef0a520d37219bf7469fe7e
StuffGames <jsanchezroa@umass.edu>
2022-09-29 10:55:49 -0400

    Sixth Commit
    Worked on a skybox change that will be called when an Event button is clicked to change the surrounding environment.
    Still needs a bit more work...

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -8,9 +8,31 @@ public class SceneManager : MonoBehaviour
     public EventContentList e;
     public List<Material> skyboxes;
 
+    private int i = 0;
+    private float lerpTime = 0.1f;
+
+    void Update(){
+    }
+
     //If button is selected, turn off until other button selected from the events list
     public void ButtonSelection(GameObject button){
         button.GetComponent<Button>().interactable = false;
         e.SwitchEvents(button.GetComponent<RectTransform>());
+        SkyBoxChange();
+    }
+
+    //ISSUE SkyBoxChange gets called once from the button click but does not update the interpolation every frame
+    //find some way to move the interpolation to Update function
+
+    //Changes the skybox based on the button clicked
+    public void SkyBoxChange(){
+        Material changeM = new Material(RenderSettings.skybox);
+        RenderSettings.skybox = changeM;
+        changeM.Lerp(RenderSettings.skybox, skyboxes[i], lerpTime);
+        //When interpolation finished...
+        //RenderSettings.skybox = skyboxes[i];
+        i++;
+        DynamicGI.UpdateEnvironment();
+        if (i >= skyboxes.Count) i = 0;
     }
 }
