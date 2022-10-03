af5131d1f2ba1e64693539249c71a8c19e2b90ef
StuffGames <jsanchezroa@umass.edu>
2022-10-03 16:55:08 -0400

    7th Commit
    Rewrote the skybox script, now it works as intended, mostly.
    Still needs a bit more work but for the most part is doing good.
    Started transitioning into VR Testing

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -8,31 +8,46 @@ public class SceneManager : MonoBehaviour
     public EventContentList e;
     public List<Material> skyboxes;
 
-    private int i = 0;
-    private float lerpTime = 0.1f;
+    private int i = 1;
+    private float lerpTime = 0.9f;
+
+    private bool mFlag = false;
+
+    private Material m;
+
+    void Start() {
+        m = new Material(RenderSettings.skybox);
+    }
 
     void Update(){
+        SkyBoxChange();
     }
 
     //If button is selected, turn off until other button selected from the events list
     public void ButtonSelection(GameObject button){
         button.GetComponent<Button>().interactable = false;
         e.SwitchEvents(button.GetComponent<RectTransform>());
-        SkyBoxChange();
+        //m = new Material(RenderSettings.skybox);
+        m.name = "Temporary Material";
+        RenderSettings.skybox = m;
+        
+        i++;
+        if (i >= skyboxes.Count) i = 0;
+        mFlag = true;
     }
 
-    //ISSUE SkyBoxChange gets called once from the button click but does not update the interpolation every frame
-    //find some way to move the interpolation to Update function
+    float lTime = 0; //counting from interpolation start to end
 
-    //Changes the skybox based on the button clicked
-    public void SkyBoxChange(){
-        Material changeM = new Material(RenderSettings.skybox);
-        RenderSettings.skybox = changeM;
-        changeM.Lerp(RenderSettings.skybox, skyboxes[i], lerpTime);
-        //When interpolation finished...
-        //RenderSettings.skybox = skyboxes[i];
-        i++;
-        DynamicGI.UpdateEnvironment();
-        if (i >= skyboxes.Count) i = 0;
+    //Changes the skybox over time
+    private void SkyBoxChange(){
+        if (mFlag){
+            m.Lerp(m, skyboxes[i], lerpTime * Time.deltaTime);
+            DynamicGI.UpdateEnvironment();
+            lTime += lerpTime * Time.deltaTime;
+        }
+        if (lTime >= 1.5){
+            mFlag = false;
+            lTime = 0;
+        }
     }
 }
